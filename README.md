497b788c47d0dc71c68018a9ba48c343110c0d96
Shubham Patil <shubham.patil@espressif.com>
2024-05-02 14:37:07 +0530

    Minor restructuring and changes required to upload rainmaker cli to pypi

---
+++ README.md
@@ -0,0 +1,30 @@
+# ESP RainMaker Command Line utility
+
+Welcome to the `esp-rainmaker-cli` repository!
+
+This repository contains the source of ESP RainMaker Command Line utility.
+
+## About RainMaker
+
+[ESP RainMaker](https://github.com/espressif/esp-rainmaker)
+is an end-to-end solution offered by Espressif to enable remote control and
+monitoring for ESP32 based products without any configuration required in the Cloud.
+
+## How to install
+
+[esp-rainmaker-cli](https://pypi.org/project/esp-rainmaker-cli) is available on Python Package Index (PyPI).
+It can be installed using pip.
+
+```
+python3 -m pip install esp-rainmaker-cli
+```
+
+## Usage
+
+Please check the [CLI Usage guide](https://rainmaker.espressif.com/docs/cli-usage.html) for more information.
+
+For more help, you can also run the following command:
+
+```
+esp-rainmaker-cli --help
+```
---
+++ setup.py
@@ -0,0 +1,85 @@
+#-!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import sys
+
+def get_install_requires():
+    with open(os.path.realpath('requirements.txt')) as f:
+        required = f.read().splitlines()
+        return required
+
+try:
+    from setuptools import find_packages, setup
+except ImportError:
+    print(
+        "Package setuptools is missing from your Python installation. "
+        "Please see the installation section in the esp-matter-mfg-tool "
+        "documentation for instructions on how to install it."
+    )
+    exit(1)
+
+VERSION = "1.0.0"
+
+long_description = """
+=================
+esp-rainmaker-cli
+=================
+ESP RainMaker command-line interface (CLI) utility, python utility to perform host based claiming.
+
+Source code for `esp-rainmaker-cli` is
+`hosted on github <https://github.com/espressif/esp-rainmaker-cli>`_.
+
+Documentation
+-------------
+Visit online `RainMaker CLI setup and usage guide <https://rainmaker.espressif.com/docs/cli-setup>`_.
+Or run `esp-rainmaker-cli -h`.
+
+License
+-------
+The License for the project can be found
+`here <https://github.com/espressif/esp-rainmaker-cli/tree/master/LICENSE>`_
+"""
+
+setup(
+    name = "esp-rainmaker-cli",
+    version = VERSION,
+    description = "A python utility to perform host based claiming",
+    long_description = long_description,
+    long_description_content_type = 'text/x-rst',
+    url = "https://github.com/espressif/esp-rainmaker-cli",
+
+    project_urls = {
+        "Documentation": "https://rainmaker.espressif.com/docs/cli-setup",
+        "Source": "https://github.com/espressif/esp-rainmaker-cli",
+    },
+
+    author = "Espressif Systems",
+    author_email = "",
+    license = "Apache-2.0",
+
+    classifiers = [
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Software Development :: Embedded Systems",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+
+    python_requires = ">=3.9",
+    setup_requires = (["wheel"] if "bdist_wheel" in sys.argv else []),
+    install_requires = get_install_requires(),
+    include_package_data = True,
+    packages = find_packages(),
+
+    entry_points={
+        'console_scripts': [
+            'esp-rainmaker-cli = rainmaker.rainmaker:main',
+        ],
+    },
+)
